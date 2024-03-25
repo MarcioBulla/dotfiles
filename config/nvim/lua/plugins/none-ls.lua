dc6adef08ba895fd47663bf4fdfb6fb19829f21d
Marcio Bulla <marciobulla@ufpr.br>
2024-03-25 18:00:12 -0300

    updates

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -4,15 +4,15 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.fixjson,
-        null_ls.builtins.diagnostics.jsonlint,
+        -- null_ls.builtins.formatting.fixjson,
+        -- null_ls.builtins.diagnostics.jsonlint,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.clang_format,
         null_ls.builtins.diagnostics.cmake_lint,
         null_ls.builtins.diagnostics.yamllint,
-        null_ls.builtins.formatting.latexindent,
+        -- null_ls.builtins.formatting.latexindent,
         null_ls.builtins.formatting.yamlfix,
         null_ls.builtins.diagnostics.yamllint,
       },
