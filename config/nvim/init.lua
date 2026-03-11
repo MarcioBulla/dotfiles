8ad41ac6c719af01fb31a494db0071f9eb902191
Marcio Bulla <marciobullajr@gmail.com>
2026-03-11 13:34:46 -0300

    add bash and hyprlang to lsp

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,5 @@
-require('config.keymaps')
-require('config.vim-configs')
-require('config.lazy')
+require("config.keymaps")
+require("config.vim-configs")
+require("config.lazy")
 
-vim.lsp.enable({ "zls", "ltex_local", "clangd", "pyright", "lua_ls" })
+vim.lsp.enable({ "zls", "ltex_local", "clangd", "pyright", "lua_ls", "hyprls", "bashls" })
